28d41f2a60760d143a90a45933a4032be82019d6
Ubuntu <cerenchun@miuandes.cl>
2019-06-18 12:30:04 -0400

    pagina web con nav bar y logo

--- README.md
+++ README.md
@@ -6,19 +6,12 @@ application up and running.
 Things you may want to cover:
 
 * Ruby version
+ruby 2.5.1p57 (2018-03-29 revision 63029) [x86_64-linux]
 
-* System dependencies
 
-* Configuration
+
 
 * Database creation
+ creados en seed
 
-* Database initialization
-
-* How to run the test suite
-
-* Services (job queues, cache servers, search engines, etc.)
-
-* Deployment instructions
-
-* ...
+en vez de bajar bobstrap utilice la gema bulma 
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  get 'home_controller/index'
   resources :order_products
   resources :products
   resources :add_shippings
@@ -7,6 +8,8 @@ Rails.application.routes.draw do
   devise_for :users
   get 'catalog/index'
 
-  root to: "home#index"
+  root to: "home_controller#index"
+  get '/users/home_controller', to: "home_controller#index"
+  get '/home_controller', to: "home_controller#index"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
